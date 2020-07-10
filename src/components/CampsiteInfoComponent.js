fae3fa855fe21b0e1aeb2bdcb4ba426e79bff1fc
Maiyo <maiyopabz@gmail.com>
2020-07-10 02:01:47 -0700

    Add a new file named MainComponent.js

---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+
+
+class CampsiteInfo extends Component {
+    renderCampsite (campsite) {
+        if(campsite){
+            return(
+                <div className="col-md-5 m-1">
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+                </div>
+            );
+        }
+        return <div />
+    }
+
+    renderComments(comments){
+        if(comments){
+            return(
+                <div className="col-m-5 m-1">
+                    <h4> Comments: </h4>
+                    {comments.map(comment => {
+                        return <div key={comments.id}>
+                            <p>{comment.text}</p>
+                            <p>{comment.author}
+                            {new Intl.DateTimeFormat ('en-US',{
+                            year:'numeric', 
+                            month:'short',
+                            day:'2-digit'
+                            }).format(new Date(Date.parse(comment.date)
+                            ))}</p>
+                            </div>          
+                    })
+                }
+                </div>
+            )
+        } 
+    }
+        
+    render(){
+        if (this.props.campsite){
+            return(
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
+                </div>
+            );
+        } 
+        return (<div />);
+       
+    }
+}
+    
+
+export default CampsiteInfo;
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'typeface-lobster';
 import 'typeface-open-sans';
+import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 
 ReactDOM.render( < App / > , document.getElementById("root"));
