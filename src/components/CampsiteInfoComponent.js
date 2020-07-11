8a96f0874a09d5fa64e0ffcc2731695dd3339b13
Maiyo <maiyopabz@gmail.com>
2020-07-11 02:26:01 -0700

    Add Header and Footer components to render the corresponding views in the app, using Font-Awesome and Bootstrap-Social icons, and add new CSS rules.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,63 +1,56 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
 
-class CampsiteInfo extends Component {
-    renderCampsite (campsite) {
-        if(campsite){
+
+function RenderCampsite ({campsite}) {      
             return(
                 <div className="col-md-5 m-1">
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
+                    <Card>
+                        <CardImg top src={campsite.image} alt={campsite.name} />
+                        <CardBody>
+                            <CardTitle>{campsite.name}</CardTitle>
+                            <CardText>{campsite.description}</CardText>
+                        </CardBody>
+                    </Card>
                 </div>
             );
-        }
-        return <div />
     }
 
-    renderComments(comments){
+function RenderComments({comments}){
         if(comments){
             return(
                 <div className="col-m-5 m-1">
                     <h4> Comments: </h4>
                     {comments.map(comment => {
-                        return <div key={comments.id}>
-                            <p>{comment.text}</p>
-                            <p>{comment.author}
-                            {new Intl.DateTimeFormat ('en-US',{
-                            year:'numeric', 
-                            month:'short',
-                            day:'2-digit'
-                            }).format(new Date(Date.parse(comment.date)
-                            ))}</p>
+                        return (
+                            <div key={comments.id}>
+                                <p>{comment.text}</p>
+                                <p>{comment.author}, {new Intl.DateTimeFormat ('en-US',{year:'numeric', month:'short',day:
+                                '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                </p>
                             </div>          
-                    })
-                }
+                        );
+                    })}
                 </div>
-            )
-        } 
+            );
+        }
+        return <div />; 
     }
         
-    render(){
-        if (this.props.campsite){
+    function CampsiteInfo(props){
+        if (props.campsite){
             return(
                 <div className="container">
                     <div className="row">
-                        {this.renderCampsite(this.props.campsite)}
-                        {this.renderComments(this.props.campsite.comments)}
+                        <RenderCampsite campsite={props.campsite}/>
+                        <RenderComments comments={props.campsite.comments} />
                     </div>
                 </div>
             );
         } 
-        return (<div />);
-       
-    }
+        return (<div/>);
 }
     
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'typeface-lobster';
 import 'typeface-open-sans';
-import * as serviceWorker from "./serviceWorker";
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from "./App";
 
 ReactDOM.render( < App / > , document.getElementById("root"));
