9fce8fb7780d4ca7dea44786538ad1476d9f5aeb
Maiyo <maiyopabz@gmail.com>
2020-07-21 04:14:58 -0700

    I set up a file to hold action types, as well as a file to hold action creators, and set up an action to be dispatched to the comments reducer to update the Redux store. I learned to then dispatch that action through setting up a mapDispatchToProps for use in the connect() function.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -63,8 +63,7 @@ function CampsiteInfo(props) {
                 </div>
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
-                        comments={props.comments}
+                    <RenderComments comments={props.comments}
                         addComment={props.addComment}
                         campsiteId={props.campsite.id}
                     />
@@ -88,8 +87,8 @@ class CommentForm extends Component {
     };
 
     handleSubmit(values) {
-      console.log("Current state is: " + JSON.stringify(values));
-      alert("Current state is: " + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text)
     }
     
     toggleModal() {
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Contact from "./ContactComponent";
 import About from "./AboutComponent";
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
 
 
 const mapStateToProps = state => {
@@ -16,7 +17,11 @@ const mapStateToProps = state => {
         comments: state.comments,
         partners: state.partners,
         promotions: state.promotions
-    };
+    }
+}
+
+const mapDispatchToProps = {
+  addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
 };
 
 class Main extends Component {
@@ -40,14 +45,11 @@ class Main extends Component {
     const CampsiteWithId = ({ match }) => {
       return (
         <CampsiteInfo
-          campsite={
-            this.props.campsites.filter(
-              (campsite) => campsite.id === +match.params.campsiteId
-            )[0]
-          }
-          comments={this.props.comments.filter(
-            (comment) => comment.campsiteId === +match.params.campsiteId
-          )}
+          campsite={this.props.campsites.filter((campsite) => campsite.id === 
+          +match.params.campsiteId)[0]}
+          comments={this.props.comments.filter((comment) => comment.campsiteId === 
+          +match.params.campsiteId)}
+          addComment={this.props.addComment}
         />
       );
     };
@@ -69,4 +71,4 @@ class Main extends Component {
   }
 }
 
-export default (connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
