b875607b3bb3f532cb3694eb3dadcdcba46d1e4d
Maiyo <maiyopabz@gmail.com>
2020-07-24 20:43:27 -0700

    I set up action creators to request resources from json-server using the Fetch API.

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -9,13 +9,14 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 function RenderDirectoryItem({campsite}) {
   return (
     <Card>
     <Link to={`/directory/${campsite.id}`}>
-    <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+    <CardImg width="100%" src={baseUrl + campsite.image} alt={campsite.name} />
     <CardImgOverlay>
       <CardTitle>{campsite.name}</CardTitle>
     </CardImgOverlay>
